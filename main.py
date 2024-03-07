f837bc9f6c0fdaa91aa50358c17ef792eecd7966
Ashmit Thawait <75786013+ashmit0920@users.noreply.github.com>
2024-03-08 02:25:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from twilio.rest import Client
 import os
 from dotenv import load_dotenv
 
+# extract your api credentials from a .env file using the load_dotenv() function. You can also set them as environment variables and use os.environ() method instead.
+
 def send_message(number, amount, item):
     
-    #load_dotenv()
-    account_sid = 'AC151e0515d07100e64726d6dff5ef286f'
-    auth_token = '9a3d7206573c3ad4cf5b730ac3c8a6d1'
+    account_sid = 'YOUR_SID'
+    auth_token = 'YOUR_AUTH_TOKEN'
     client = Client(account_sid, auth_token)
 
     message = client.messages.create(
-    from_='whatsapp:+14155238886',
+    from_='whatsapp:YOUR_TWILIO_PHONE_NUMBER',
     body=f'Thank you for shopping with us! \nYour purchased item: \n{item}.\n\nTotal amount paid: Rs. {amount}/-',
     to=f'whatsapp:+91{number}'
     )
